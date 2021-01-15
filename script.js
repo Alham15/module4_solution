6974639438d5800cee05d05ebfc5c724137b058d
Alham Akram <alhamakram2002@gmail.com>
2021-01-16 01:20:24 +0530

    Update

---
+++ script.js
@@ -0,0 +1,24 @@
+
+(function (window){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+  for (var i = 0; i < names.length; i++) {
+   
+    
+    a=names[i].charAt(0);
+    if (a==="j"||a==="J") {
+
+      byeSpeaker.speak(names[i]);
+      
+
+    }
+
+    else{
+      helloSpeaker.speak(names[i]);
+      
+    }
+  };
+ 
+
+}
+)(window);
